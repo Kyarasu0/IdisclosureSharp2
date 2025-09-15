113eeecf7c499fb76fc408b217a10f4ef3a3d4ba
toaru05 <toaru8079@gmail.com>
2025-09-15 21:58:17 +0900

    MoveTo02: Done
    Assignee: toaru
    Path: MainPrograms/01_StartClick/Move&Save/MoveTo02.cs
    Linear: Closes IDI-28
    Description: 左クリックで01_StartClickから02_InputProfilesへの遷移

--- Idisclosure#2/Assets/Projects/[P]MainPrograms/[P]01_StartClick/Move&Save/MoveTo02.cs
+++ Idisclosure#2/Assets/Projects/[P]MainPrograms/[P]01_StartClick/Move&Save/MoveTo02.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
-
-public class MoveTo02
+using UnityEngine.SceneManagement;
+public class MoveTo02 : MonoBehaviour
 {
-    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("02_InputProfiles");
+        }
+    }
 }
