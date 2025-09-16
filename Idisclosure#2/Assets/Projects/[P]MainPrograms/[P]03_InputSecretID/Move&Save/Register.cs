94c141d3d5549a6a03ee2a27fe65135841816a79
toaru05 <toaru8079@gmail.com>
2025-09-17 03:34:30 +0900

    Register：Done
    Assignee: toaru
    Path: 03_InputSecretID/Move&Save/Register.cs
    Linear: Closes IDI-31
    Description:  PlayerPrefesへ保存、04_Createjoinへの遷移

--- Idisclosure#2/Assets/Projects/[P]MainPrograms/[P]03_InputSecretID/Move&Save/Register.cs
+++ Idisclosure#2/Assets/Projects/[P]MainPrograms/[P]03_InputSecretID/Move&Save/Register.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
 
-public class Register
+public class Register : MonoBehaviour
 {
-    
+    public TMP_InputField SecretIDInputField;
+    public TextMeshProUGUI TokensText;
+    public Button submitButton;
+    private Regex secretIDRegex = new Regex(@"^[A-Za-z0-9!#$%&'-=^~|@`;+:*,<.>/?_]+$");
+
+    void Start()
+    {
+        submitButton.onClick.AddListener(OnSubmit);
+
+    }
+
+    void OnSubmit()
+    {
+        string SecretID = SecretIDInputField.text;
+        string Tokens = TokensText.text;
+
+        if (!secretIDRegex.IsMatch(SecretID))
+        {
+            Debug.Log("Invalid Secret ID format.");
+            return;
+        }
+        Debug.Log("Secret ID and Tokens saved.");
+        PlayerPrefs.SetString("SecretID", SecretID);
+        PlayerPrefs.SetString("Tokens", Tokens);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("04_CreateJoin");
+    }
 }
