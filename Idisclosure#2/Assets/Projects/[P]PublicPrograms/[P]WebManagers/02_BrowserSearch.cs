de9afab76c79bc67f83689751e5b5f63ce9433ac
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-09-19 05:12:31 +0900

    CreateStoreWeb, BrowserSearch:  Done
    Assignee: Kyarasu
    Path: PublicPrograms/WebManager/CreateStoreWeb
    PublicPrograms/WebManager/BrowserSeach
    Linear: Closes IDI-63, Closes IDI-64
    Description: 毎秒確率をひいてWebが建ち上がるスクリプト、webが存在していたら検索にひっかかるスクリプトを作成しました。

---
+++ Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]WebManagers/02_BrowserSearch.cs
@@ -0,0 +1,160 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using System.Linq;
+using System;
+using System.Net;
+
+public class BrowserSearch : MonoBehaviour
+{
+    public TMP_Text SearchWords;
+    public string PC_Or_Server;
+    string WitchBattery;
+    string WitchIP;
+    string[] Webs;
+    [Header("検索に使用するBattery")]
+    public int Drain;
+
+    public void SearchAndMove()
+    {
+        // 検索ワードの圧縮化
+        string searchWords = SearchWords.text.Trim().ToLower().Replace("\u200B", "").Replace(" ", "");
+        Debug.Log("Browser起動:" + searchWords);
+
+        // BatteryとIPの設定
+        if (PC_Or_Server == "PC")
+        {
+            WitchBattery = "Battery";
+            WitchIP = "PlayerIP";
+        }
+        else if (PC_Or_Server == "Server")
+        {
+            WitchBattery = "BatteryMyServer";
+            WitchIP = "ServerIP";
+        }
+
+        foreach (string web in Webs)
+        {
+            /*----------検索----------*/
+            // データとwebの存在を確認
+            if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey(web) && (bool)PhotonNetwork.CurrentRoom.CustomProperties[web] || searchWords == "SNSServer".ToLower())
+            {
+                if (searchWords == web.ToLower())
+                {
+                    // Battery関係を設定
+                    int Battery = PlayerPrefs.GetInt(WitchBattery, 0);
+
+                    if ((Battery - Drain >= 0))
+                    {
+                        // バッテリー減算処理
+                        Battery -= Drain;
+                        PlayerPrefs.SetInt(WitchBattery, Battery);
+                        PlayerPrefs.Save();
+
+                        // Wifi番号を取得
+                        int WifiNumber = PlayerPrefs.GetInt("WifiNumber", 1);
+
+                        // 送信元IPを取得
+                        string IP = "";
+                        if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey(WitchIP))
+                        {
+                            IP = (string)PhotonNetwork.LocalPlayer.CustomProperties[WitchIP];
+                        }
+
+                        // Phishingの場合
+                        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("Phishing" + web) && (bool)PhotonNetwork.CurrentRoom.CustomProperties["Phishing" + web])
+                        {
+                            // PhishingWebIPを取得
+                            string PhishingWebIP = "";
+                            if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("Phishing" + web + "IP"))
+                            {
+                                FishingWebIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["Phishing" + web + "IP"];
+                            }
+
+                            // WiFiに保存
+                            SortAndSave(PhishingWebIP, IP, WifiNumber);
+
+                            // Scene遷移
+                            SceneManager.LoadScene("Phishing" + web);
+                        }
+                        // SNS Serverの場合
+                        else if (searchWords == "SNSServer".ToLower())
+                        {
+                            // SNSServerIPを取得
+                            string snsServerIP = "";
+                            if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("SNSServerIP"))
+                            {
+                                snsServerIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["SNSServerIP"];
+                            }
+
+                            // WiFiに保存
+                            SortAndSave(snsServerIP, IP, WifiNumber);
+
+                            // Scene遷移
+                            SceneManager.LoadScene("SNSServer");
+                        }
+                        // Phishingではないの場合
+                        else
+                        {
+                            // WebIPを取得
+                            string WebIP = "";
+                            if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey(web + "IP"))
+                            {
+                                WebIP = (string)PhotonNetwork.CurrentRoom.CustomProperties[web + "IP"];
+                            }
+
+                            // WiFiに保存
+                            SortAndSave(WebIP, IP, WifiNumber);
+
+                            // Scene遷移
+                            SceneManager.LoadScene(web);
+                        }
+                    }
+
+                }
+            }
+                    
+        }
+    }
+
+    public void SortAndSave(string IP1, string IP2, int WifiNumber)
+    {
+        int Permutation = UnityEngine.Random.Range(0, 2);
+
+        string NewRecord = (Permutation == 0) ? $"{IP1}:{IP2}\n" : $"{IP2}:{IP1}\n";
+
+        // 既存のWiFi記録を取得して追加
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("WiFi" + WifiNumber))
+        {
+            string Record = (string)PhotonNetwork.CurrentRoom.CustomProperties["WiFi" + WifiNumber];
+            NewRecord = Record + NewRecord;
+        }
+
+        // カスタムプロパティを更新
+        Hashtable props = new Hashtable
+        {
+            { "WiFi" + WifiNumber, NewRecord },
+        };
+        PhotonNetwork.CurrentRoom.SetCustomProperties(props);
+    }
+    
+    private void LoadWebs()
+    {
+        // Jsonのパスを指定してテキスト形式で読み取り
+        TextAsset jsonFile = Resources.Load<TextAsset>("webs");
+        if (jsonFile != null)
+        {
+            // 正しい形式にしてconfigに保存、そのconfigの読み出し
+            WebsConfig config = JsonUtility.FromJson<WebsConfig>(jsonFile.text);
+            Webs = config.webs;
+        }
+        else
+        {
+            Debug.LogError("Webs config not found!");
+            Webs = new string[0];
+        }
+    }
+}
+
