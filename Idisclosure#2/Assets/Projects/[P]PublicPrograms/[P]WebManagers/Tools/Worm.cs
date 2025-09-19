56e115fa1a92382da19fe4ae59f4d2ec0f29e291
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-09-19 21:36:01 +0900

    Spyware, Worm: Done
    Assignee: Kyarasu
    Path: PublicPrograms/WebManagers/Tools/Spyware
    PublicPrograms/WebManagers/Tools/Worm
    Linear: Closes IDI-98, Closes IDI-99
    Description: Terminalで実行すると次のPhishingにMalwareが仕掛けられます。

---
+++ Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]WebManagers/Tools/Worm.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Worm : MonoBehaviour
+{
+    public void Execute(string command, string WitchBattery)
+    {
+        // 消費するBatteryを設定
+        int drain = 10;
+        // Battery情報を取得
+        int battery = PlayerPrefs.GetInt(WitchBattery, 0);
+        // Battery的に起動可能な場合
+        if (battery - drain >= 0)
+        {
+            // Battery処理
+            battery -= drain;
+            PlayerPrefs.SetInt(WitchBattery, battery);
+            PlayerPrefs.Save();
+
+            // 実行
+            PlayerPrefs.SetInt("HaveWorm", 1);
+            PlayerPrefs.Save();
+            Debug.Log("Wormをゲットした!");
+
+            // 表示
+            PlayerPrefs.SetString("TerminalDisplay", "The worm is ready on the phishing server!\n\n");
+            PlayerPrefs.Save();
+        }
+    }
+}
