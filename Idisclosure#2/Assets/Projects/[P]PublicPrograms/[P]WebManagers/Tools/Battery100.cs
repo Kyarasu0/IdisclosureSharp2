bbcedfb57da94752111b3aca14891df60db9ff1c
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-09-19 22:18:14 +0900

    Battery25, Battery100: Done
    Assignee: Kyarasu
    Path: PublicPrograms/WebManagers/Tools/Battery25
    PublicPrograms/WebManagers/Tools/Battery100
    Linear: Closes IDI-100, Closes IDI-101
    Description: Battery25 --PCかBattery --ServerのようにしてTerminalで使います

---
+++ Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]WebManagers/Tools/Battery100.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Battery100 : MonoBehaviour
+{
+    public void Execute(string command, string WitchBattery)
+    {
+        // 消費するBatteryを設定
+        int add = 100;
+        // commandからPCのBatteryを増やすのかServerのBatteryを増やすのかを判別
+        if (command == "--PC" || command == "--Server")
+        {
+            // Batteryの情報を取得
+            int battery = PlayerPrefs.GetInt("BatteryMy" + command.Replace("-", ""), 0);
+
+            // 実行
+            battery += add;
+            PlayerPrefs.SetInt("BatteryMy" + command.Replace("-", ""), battery);
+            PlayerPrefs.Save();
+
+            // 表示
+            PlayerPrefs.SetString("TerminalDisplay", "Battery increased by 100%!");
+            PlayerPrefs.Save();
+        }
+    }
+}
