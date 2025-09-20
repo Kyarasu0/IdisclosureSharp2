6a07f90f6056d26e58bcb9b9b07ec3d8252be454
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-09-21 01:56:36 +0900

    InitialReset: Done
    Assignee: Kyarasu
    Path: MainPrograms/Waiting/M&S/InitialReset
    Description: ポート番号が表示されるようになりました！

---
+++ Idisclosure#2/Assets/Projects/[P]MainPrograms/[P]05_Waiting/Move&Save/InitialResets.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class InitialResets : MonoBehaviour
+{
+    string[] Webs;
+    void Start()
+    {
+        LoadWebs();
+    }
+    public void ResetButton()
+    {
+        // PlyerPrefs
+        PlayerPrefs.SetInt("Battery", 100);
+        PlayerPrefs.SetInt("BatteryMyServer", 100);
+        PlayerPrefs.SetInt("PhishingNow", 0);
+        PlayerPrefs.Save();
+
+        // Hashtables
+        Hashtable props = new Hashtable
+        {
+            // ServerSecurity
+            { "Port", 22 },
+            { "Password", 000 },
+        };
+        // WebManagers
+        Hashtable webs = new Hashtable();
+        foreach (string Web in Webs)
+        {
+            webs[Web] = false;
+            webs[$"Phishing{Web}"] = false;
+        }
+        PhotonNetwork.CurrentRoom.SetCustomProperties(webs);
+        PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+    }
+
+    private void LoadWebs()
+    {
+        // Jsonのパスを指定してテキスト形式で読み取り
+        TextAsset jsonFile = Resources.Load<TextAsset>("webs");
+        if (jsonFile != null)
+        {
+            // 正しい形式にしてconfigに保存、そのconfigの読み出し
+            WebsConfig config = JsonUtility.FromJson<WebsConfig>(jsonFile.text);
+            Webs = config.webs;
+        }
+        else
+        {
+            Debug.LogError("Webs config not found!");
+            Webs = new string[0];
+        }
+    }
+}
