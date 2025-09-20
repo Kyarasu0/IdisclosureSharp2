2a1bf59bc0a38a5a4c1420f6bef92f38d71d1453
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-09-20 15:33:57 +0900

    PublicPrograms: Done
    Assignee: Kyarasu
    Description: ところどころ細かい変更のみ行っていますがあとはほとんど大丈夫です。

--- Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]Observers/CrackedManager.cs
+++ Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]Observers/CrackedManager.cs
@@ -16,6 +16,14 @@ public class CrackedManager : MonoBehaviourPunCallbacks, IOnEventCallback
             string message = (string)photonEvent.CustomData;
             Debug.Log("[CrackedManager] Message received: " + message);
 
+            // Tekensをリセット
+            PlayerPrefs.SetInt("Tokens",0);
+            Hashtable Reset  = new Hashtable
+            {
+                {"Tokens", 0},
+            };
+            PhotonNetwork.LocalPlayer.SetCustomProperties(Reset);
+
             // Scene遷移
             SceneManager.LoadScene("Cracked");
         }
