70e97714b5b751d5b435ff5cf091188212f818ff
toaru05 <toaru8079@gmail.com>
2025-09-20 14:28:31 +0900

    PlayerPrefController: Done
    Assignee: toaru
    Path: PublicPrograms/Status/UserInformation/PlayerPrefController.cs
    Description: tokensとbatteryをint型にした

--- Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]Status/UserInformation/PlayerPrefController.cs
+++ Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]Status/UserInformation/PlayerPrefController.cs
@@ -15,8 +15,8 @@ public class PlayerPrefsController : MonoBehaviourPunCallbacks
         // 自分の情報をPlayerPrefsから取得
         string name = PlayerPrefs.GetString("Name", "name").Replace("\u200B", "");
         string secretID = PlayerPrefs.GetString("SecretID", "secret").Replace("\u200B", "");
-        string tokens = PlayerPrefs.GetString("Tokens", "0").Replace("\u200B", "");
-        string battery = PlayerPrefs.GetString("Battery", "0").Replace("\u200B", "");
+        int tokens = PlayerPrefs.GetInt("Tokens", 0);
+        int battery = PlayerPrefs.GetInt("Battery", 0);
         string birthday = PlayerPrefs.GetString("Birthday", "0101").Replace("\u200B", "");
         string birthyear = PlayerPrefs.GetString("Birthyear", "2000").Replace("\u200B", "");
         int age = PlayerPrefs.GetInt("Age", 00);
@@ -24,24 +24,24 @@ public class PlayerPrefsController : MonoBehaviourPunCallbacks
         // UIに表示
         if (TokensText != null)
         {
-            TokensText.text = tokens;
+            TokensText.text = tokens.ToString();
         }
         if (BatteryText != null)
         {
-            if (int.Parse(battery) > 100)
+            if (battery > 100)
             {
-                battery = "100";
-                BatteryText.text = battery;
+                battery = 100;
+                BatteryText.text = battery.ToString();
             }
-            else if (int.Parse(battery) <= 0)
+            else if (battery <= 0)
             {
-                battery = "0";
-                BatteryText.text = battery;
+                battery = 0;
+                BatteryText.text = battery.ToString();
                 SceneManager.LoadScene("EndGame");
             }
             else
             {
-                BatteryText.text = battery;
+                BatteryText.text = battery.ToString();
             }
             
         }
