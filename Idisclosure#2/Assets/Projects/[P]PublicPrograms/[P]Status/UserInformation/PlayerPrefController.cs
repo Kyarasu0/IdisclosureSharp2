5a010e25b358a1cd4d931c93c521a3628650ab21
toaru05 <toaru8079@gmail.com>
2025-09-20 14:10:43 +0900

    Ranking, PlayerPrefController: Done
    Assignee: toaru
    Path: MainPrograms/08_Ranking/functions/Ranking.cs
    Linear: Closes IDI-58, Closes IDI-84
    Description: PlayerPrefController.csとRanking.csの作成

--- Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]Status/UserInformation/PlayerPrefController.cs
+++ Idisclosure#2/Assets/Projects/[P]PublicPrograms/[P]Status/UserInformation/PlayerPrefController.cs
@@ -1,6 +1,63 @@
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
-public class PlayerPrefController
+public class PlayerPrefsController : MonoBehaviourPunCallbacks
 {
-    
+    public TMP_Text TokensText;
+    public TMP_Text BatteryText;
+
+    void Update()
+    {
+        // 自分の情報をPlayerPrefsから取得
+        string name = PlayerPrefs.GetString("Name", "name").Replace("\u200B", "");
+        string secretID = PlayerPrefs.GetString("SecretID", "secret").Replace("\u200B", "");
+        string tokens = PlayerPrefs.GetString("Tokens", "0").Replace("\u200B", "");
+        string battery = PlayerPrefs.GetString("Battery", "0").Replace("\u200B", "");
+        string birthday = PlayerPrefs.GetString("Birthday", "0101").Replace("\u200B", "");
+        string birthyear = PlayerPrefs.GetString("Birthyear", "2000").Replace("\u200B", "");
+        int age = PlayerPrefs.GetInt("Age", 00);
+
+        // UIに表示
+        if (TokensText != null)
+        {
+            TokensText.text = tokens;
+        }
+        if (BatteryText != null)
+        {
+            if (int.Parse(battery) > 100)
+            {
+                battery = "100";
+                BatteryText.text = battery;
+            }
+            else if (int.Parse(battery) <= 0)
+            {
+                battery = "0";
+                BatteryText.text = battery;
+                SceneManager.LoadScene("EndGame");
+            }
+            else
+            {
+                BatteryText.text = battery;
+            }
+            
+        }
+
+        // カスタムプロパティに設定
+        Hashtable props = new Hashtable();
+        props["Name"] = name;
+        props["SecretID"] = secretID;
+        props["Tokens"] = tokens;
+        props["Battery"] = battery;
+        props["Birthday"] = birthday;
+        props["Birthyear"] = birthyear;
+        props["Age"] = age;
+
+        // Photonのネットワークに保存
+        PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+    }
 }
+
